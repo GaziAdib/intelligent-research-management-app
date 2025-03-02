6a415a4adb04ee067b82600647dec09b9fc289ef
unknown <adibcse10@gmail.com>
2025-03-02 17:46:30 +0600

    added some folder structure for api libs and services

---
+++ app/libs/permissions.js
@@ -0,0 +1,4 @@
+export const permissions = {
+    LEADER: ['add_member', 'remove_member', 'edit_member_role', 'add_task', 'remove_task', 'edit_task', 'approve_task', 'reject_task', 'add_note_to_task'],
+    MEMBER: ['modify_task_content', 'view_tasks', 'edit_profile', 'exit_team']
+}
